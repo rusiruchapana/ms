2399a012dca2bd58021ac72d3541a751a116b607
Rusiru chapana <rusiru.chapana@atlato.com>
2025-12-02 21:47:30 +0530

    Start the controller to create department API.

--- employee-management/department-service/src/main/java/com/em/departmentservice/controller/DepartmentController.java
+++ employee-management/department-service/src/main/java/com/em/departmentservice/controller/DepartmentController.java
@@ -1,6 +1,12 @@
 package com.em.departmentservice.controller;
 
+import com.em.departmentservice.dto.DepartmentRequestDTO;
+import com.em.departmentservice.dto.DepartmentResponseDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DepartmentController {
 
-
+    @PostMapping
+    public ResponseEntity<DepartmentResponseDTO> createDepartment(@Valid @RequestBody DepartmentRequestDTO departmentRequestDTO){
+        DepartmentResponseDTO createdDepartment =
+        return null;
+    }
 
 
 
